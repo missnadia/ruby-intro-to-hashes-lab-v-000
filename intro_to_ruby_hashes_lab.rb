8f46def75c521646e2d20563d65548ab76494c2d
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-13 21:11:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,15 +7,15 @@ def actor
 end
 
 def monopoly
+	monopoly = {railroads: {}}
+end
+
+def monopoly_with_second_tier
 	monopoly = {railroads:
 		{
 		pieces: 4
 		}
 	}
-end
-
-def monopoly_with_second_tier
-	monopoly = {railroads: {}}
 end
 
 def monopoly_with_third_tier
