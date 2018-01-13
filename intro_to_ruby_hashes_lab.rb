7dc10aa94a67267d5dadadbf64dae54ec7c1982b
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-13 21:31:19 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,8 +45,8 @@ def monopoly_with_fourth_tier
 	monopoly = {
 		:railroads=>
 		{
-			pieces: 4,
-			rent_in_dollars:
+			:pieces=> 4,
+			:rent_in_dollars=>
 			{
 				one_piece_owned: 25,
 				two_pieces_owned: 50,
